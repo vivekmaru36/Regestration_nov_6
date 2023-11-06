4ece5831e879a5b7fe41206274d468b04ed9bff6
vivekmaru36 <vivekmaru036@gmail.com>
2023-11-06 20:23:32 +0530

    Routing solved at last

--- README.md
+++ README.md
@@ -1,5 +1,20 @@
 1) Rfid 10 digits : 0006338040  
 
-2) 
+2) if rfid not 10 digits it resets the input 
+
+3) every thing is stored inside components folder
+    components -> RegestrationPage.js -> importing in app.js
+
+4) npm install react-router-dom
+
+5) made email_verification.js
+
+6) had to change RegestrationPage.js to function based it took 5 hours to debug
+
+7) use router/routes/route to link RegestrationPage.js to email_verification.js
+
+8) 
+
+
 
 
--- src/App.js
+++ src/App.js
@@ -1,177 +1,19 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import RegistrationPage from './components/RegestrationPage';
+import FourDigitNumberInput from './components/email_verification';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+// import { useNavigate } from 'react-router-dom';
 
-class RegistrationPage extends Component {
-  constructor() {
-    super();
-    this.state = {
-      firstName: '',
-      lastName: '',
-      email: '',
-      currentYear: '',
-      finalYear: '',
-      rfidno: '',
-      errors: {
-        firstName: '',
-        email: '',
-        rfidno: '',
-      }
-    };
-  }
-
-  handleInputChange = (e) => {
-    const { name, value } = e.target;
-    this.setState({ [name]: value });
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault();
-
-    const { firstName, email, rfidno } = this.state;
-    const errors = {};
-
-    // Validation for First Name
-    if (!firstName) {
-      errors.firstName = 'First Name is required';
-    }
-
-    // Validation for Email
-    if (!email) {
-      errors.email = 'Email is required';
-    } else if (!this.isValidEmail(email)) {
-      errors.email = 'Invalid email format';
-    }
-
-    // Validation for RFID no.
-    if (!rfidno || !/^[0-9]{10}$/.test(rfidno)) {
-      errors.rfidno = 'RFID no. must be 20 digits';
-
-      // clear the input fields for rfid when an error occurs of digits
-      this.setState({ rfidno:'' });
-    }
-
-    // Display confirmation dialog
-    const isConfirmed = window.confirm("Are you sure you want to submit the form?");
-
-    if (isConfirmed && Object.values(errors).every(error => !error)) {
-      // If there are no errors and the user confirms, proceed with the form submission
-      this.submitForm();
-    } else {
-      // If there are validation errors or the user cancels, update the state with errors
-      this.setState({ errors });
-    }
-  }
-
-  isValidEmail = (email) => {
-    return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-  }
-
-  submitForm = () => {
-    // Prepare the data for submission
-    const formData = {
-      firstName: this.state.firstName,
-      lastName: this.state.lastName,
-      email: this.state.email,
-      currentYear: this.state.currentYear,
-      finalYear: this.state.finalYear,
-      rfidno: this.state.rfidno
-    };
-
-    // Example: Send the form data to a server using the fetch API
-    fetch('your_server_endpoint_here', {
-      method: 'POST', // Use the appropriate HTTP method
-      headers: {
-        'Content-Type': 'application/json', // Set the appropriate content type
-      },
-      body: JSON.stringify(formData), // Convert the data to JSON
-    })
-      .then(response => {
-        if (response.ok) {
-          // Successful form submission
-          console.log('Form data submitted successfully');
-          // You can also navigate to a thank you page or perform other actions here.
-        } else {
-          // Handle errors if the submission fails
-          console.error('Form data submission failed');
-        }
-      })
-      .catch(error => {
-        console.error('An error occurred:', error);
-      });
-  }
-
-  render() {
-    const { errors } = this.state;
-
-    return (
-      <div>
-        <h2>Registration Page</h2>
-        <form onSubmit={this.handleSubmit}>
-          <div>
-            <label>First Name:</label>
-            <input
-              type="text"
-              name="firstName"
-              value={this.state.firstName}
-              onChange={this.handleInputChange}
-              required
-            />
-            {errors.firstName && <span className="error">{errors.firstName}</span>}
-          </div>
-          <div>
-            <label>Last Name:</label>
-            <input
-              type="text"
-              name="lastName"
-              value={this.state.lastName}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Email:</label>
-            <input
-              type="email"
-              name="email"
-              value={this.state.email}
-              onChange={this.handleInputChange}
-              required
-            />
-            {errors.email && <span className="error">{errors.email}</span>}
-          </div>
-          <div>
-            <label>Current Year:</label>
-            <input
-              type="number"
-              name="currentYear"
-              value={this.state.currentYear}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label>Final Year:</label>
-            <input
-              type="number"
-              name="finalYear"
-              value={this.state.finalYear}
-              onChange={this.handleInputChange}
-            />
-          </div>
-          <div>
-            <label>RFID no. (10 digits):</label>
-            <input
-              type="text"
-              name="rfidno"
-              value={this.state.rfidno}
-              onChange={this.handleInputChange}
-              required
-            />
-            {errors.rfidno && <span className="error">{errors.rfidno}</span>}
-          </div>
-          <button type="submit">Register</button>
-        </form>
-      </div>
-    );
-  }
+function App(){
+  return(
+    <Router>    
+      <Routes>
+        <Route path="/" element={<RegistrationPage/>} />
+        <Route path="/email-verification" element={<FourDigitNumberInput/>}/>
+      </Routes>
+  </Router>
+  );
 }
 
-export default RegistrationPage;
+export default App; 
